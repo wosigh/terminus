db02fc9e18287a860210289e346b90dc34a7a209
Ryan Hope <rmh3093@gmail.com>
2010-01-17 20:20:03 -0500

    make worky on webosy

--- src/bin/enterm.c
+++ src/bin/enterm.c
@@ -12,9 +12,10 @@ enterm_init(Ecore_Evas *ee, Evas_Object *term)
    t = evas_object_smart_data_get(term);
    ecore_evas_data_set(ee, "term", term);
    ecore_evas_callback_resize_set(ee, enterm_cb_resize);
-   ecore_x_icccm_size_pos_hints_set(ecore_evas_software_x11_window_get(ee), 0,
+   // XXX: PuffTheMagic
+   /*ecore_x_icccm_size_pos_hints_set(ecore_evas_software_x11_window_get(ee), 0,
 				    ECORE_X_GRAVITY_NW, 0, 0, 0, 0, 0, 0,
-				    t->font.width, t->font.height, 0.0, 0.0);
+				    t->font.width, t->font.height, 0.0, 0.0);*/
    ecore_event_handler_add(TERM_EVENT_TITLE_CHANGE,
 			   enterm_cb_title_change, ee);
    ecore_evas_resize(ee, t->w, t->h);
@@ -39,7 +40,8 @@ enterm_cb_title_change(void *data, int type, void *ev)
 
    e = ev;
    ee = data;
-   ecore_x_icccm_title_set(ecore_evas_software_x11_window_get(ee),
-			   e->title);
+   // XXX: ~PuffTheMagic
+   /*ecore_x_icccm_title_set(ecore_evas_software_x11_window_get(ee),
+			   e->title);*/
    return 1;
 }
--- src/bin/term.h
+++ src/bin/term.h
@@ -19,13 +19,13 @@
 #include <Evas.h>
 #include <Ecore_Evas.h>
 #include <Ecore_Job.h>
-#include <Ecore_X.h>
 #include "config.h"
 
 #define DEBUG1 0
 
 #define NPAR 16
-#define DATADIR PACKAGE_DATA_DIR"/"
+// XXX: PuffTheMagic
+#define DATADIR "data/"
 
 #ifdef DEBUG
 #define DPRINT(stuff) fprintf stuff
